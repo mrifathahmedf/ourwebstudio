836c3c0b628576602e794d04966586bb1009f17e
mrifathahmedf <mdrifathahmed2020@gmail.com>
2025-03-11 04:46:38 +0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,11 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // Flower Bloom Animation Delay
     setTimeout(() => {
         document.querySelector(".flower-group").style.opacity = "1";
-        document.querySelectorAll(".petal, .center").forEach((el, index) => {
-            setTimeout(() => {
-                el.style.animation = "bloom 1s ease-out forwards";
-            }, index * 500);
-        });
     }, 1000);
 
     // Dark Mode Toggle
