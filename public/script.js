ed0a01b4fad73f37cec4073e96516aae6ab425e0
mrifathahmedf <mdrifathahmed2020@gmail.com>
2025-03-10 17:57:09 +0600

    Update script.js

--- public/script.js
+++
@@ -1,9 +0,0 @@
-function openLetter() {
-    document.querySelector('.cover').style.transform = "rotateX(180deg)";
-    document.querySelector('.letter').style.opacity = "1";
-}
-
-function closeLetter() {
-    document.querySelector('.cover').style.transform = "rotateX(0deg)";
-    document.querySelector('.letter').style.opacity = "0";
-}
